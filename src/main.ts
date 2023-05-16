07ecf0438dcd685e588487188c5398d245d681ba
avery <pozxc76@pusan.ac.kr>
2023-05-17 00:05:41 +0900

    [module] cookie 모듈 추가:jy

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
 
+  const config = new DocumentBuilder()
+    .setTitle('독서')
+    .setDescription('독서 API 입니다.')
+    .setVersion('1.0.0')
+    .addBasicAuth(
+      {
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+  await app.listen(3000);
 }
 bootstrap();
