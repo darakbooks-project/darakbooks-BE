3957132b1a262a99b526f06fc8391a0af11e1572
avery <pozxc76@pusan.ac.kr>
2023-05-29 23:12:26 +0900

    [edit] cachemodule 에 redis 옵션 추가 해서 appmodule에 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserService } from './user/service/user.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import { RecordController } from './record/controller/record.controller';
 import { RecordModule } from './record/record.module';
 import configuration from './config/configuration';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: `.env/.env.${process.env.NODE_ENV || "development"}`,
     isGlobal: true, 
     load:[configuration]
-  }), AuthModule, UserModule, RecordModule, DatabaseModule],
+  }), 
+  CacheModule.register({
+    store: 'redis',
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password:process.env.REDIS_PASSWORD,
+    isGlobal:true,
+  }),
+  AuthModule, UserModule, RecordModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
