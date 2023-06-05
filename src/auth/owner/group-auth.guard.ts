b1abc6fcbff0e27057d2cd8ebececec6ee443739
avery <pozxc76@pusan.ac.kr>
2023-06-05 19:46:45 +0900

    [fix] group-auth.guard.ts 지영이 임의로 수정

--- src/auth/owner/group-auth.guard.ts
+++ src/auth/owner/group-auth.guard.ts
@@ -16,13 +16,11 @@ export class GroupAuthGuard implements CanActivate {
     const resourceId = request.params.id;
 
     // 리소스의 소유자 정보 조회(그룹장 찾는 method 연결해주세요)
-    const resource = await this.groupService. // 리소스 조회 메서드에 맞게 적절한 호출 코드로 변경해야 합니다.
+    const resourceOwner = await this.groupService.getGroupLeadById(resourceId); // 리소스 조회 메서드에 맞게 적절한 호출 코드로 변경해야 합니다.
 
-    // 요청된 리소스의 소유자 정보 (예시로 userId 필드 사용)
-    const resourceOwnerId = resource.userId;
-    console.log(resource);
+    console.log(resourceOwner);
     // 인증된 사용자의 ID와 리소스의 소유자 ID를 비교하여 본인인증 여부를 확인합니다.
-    const isOwner = userId === resourceOwnerId;
+    const isOwner = userId === resourceOwner;
     if (!isOwner) {
       throw new UnauthorizedException('Unathorized:You are not the owner of this resource.');
     }
