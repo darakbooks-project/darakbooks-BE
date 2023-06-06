5e7287f1e9db9e0c989b9519f0bb7272dc96b28f
shljessie <sl994@cornell.edu>
2023-06-06 23:17:42 +0300

    [gpt output] gpt ouput 내보내기

--- src/gpt copy/gpt.controller.ts
+++ src/gpt copy/gpt.controller.ts
@@ -17,7 +17,10 @@ import { HNSWLib } from 'langchain/vectorstores/hnswlib';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import * as fs from 'fs';
-import { loadQAStuffChain, loadQAMapReduceChain } from "langchain/chains"; 
+import { loadQAStuffChain, loadQAMapReduceChain, loadQARefineChain } from 'langchain/chains';
+import { Document } from 'langchain/document';
+import { RefineDocumentsChain } from 'langchain/chains';
+import { PromptTemplate } from 'langchain/prompts';
 // API CHAIN:  https://js.langchain.com/docs/modules/chains/other_chains/api_chain
 // CONSTITUTIONAL CHAIN : https://js.langchain.com/docs/modules/chains/other_chains/constitutional_chain
 // INPUT MODERATION CHAIN: https://js.langchain.com/docs/modules/chains/other_chains/moderation_chain
@@ -31,25 +34,63 @@ export class GPTController {
     bookdata: any[],
     @Body() userInput: any,
   ): Promise<any> {
-    const model = new OpenAI({
-      maxRetries: 10,
-      timeout: 500000,
-    });
-    const chainA = loadQAMapReduceChain(model);
+    const model = new OpenAI({ maxConcurrency: 10, temperature: 0 });
+    const chainA = loadQARefineChain(model);
 
-    bookdata.map(async (book) => {
-      const filteredContents = book[0].recomContents
-        .replace(/<\/?[^>]+(>|$)|&nbsp;/g, ' ')
-        .replace(/\s+/g, ' ');
-      book[0].recomContents = filteredContents;
-    });
+    bookdata = await Promise.all(
+      bookdata.map(async (book) => {
+        const filteredContents = book[0].recomContents
+          .replace(/<\/?[^>]+(>|$)|&nbsp;/g, ' ')
+          .replace(/\s+/g, ' ');
+        book[0].recomContents = filteredContents;
+        return new Document({
+          pageContent:
+            ' Title: ' +
+            book[0].recomTitle +
+            ' Author: ' +
+            book[0].recomAuthor +
+            ' Book Intro: ' +
+            book[0].recomContents +
+            ' Book Image: ' +
+            book[0].recomFilePath,
+        });
+      }),
+    );
+
     console.log(bookdata);
+
     const resA = await chainA.call({
       input_documents: bookdata,
-      question: userInput.userInput,
+      question:
+        userInput.userInput +
+        'in Korean from the inputDocument. Return 3 recommendations in this format: Book Title: , Author: , Book Image: ',
     });
-    console.log('ANSER: ', resA);
-    return resA;
+    console.log('CALL', resA.output_text);
+    // const cleanedText = resA.output_text.replace(/\n/g, '');
+
+    // Split the text by commas to separate the fields
+    // const entries = cleanedText.split(',');
+
+    // Format entries into JSON objects
+    // const books = entries.map((entry) => {
+    //   const bookInfo = entry.split(' - ');
+    //   const title = bookInfo[0].trim();
+    //   const author = bookInfo[1].trim();
+    //   const link = bookInfo[2].trim();
+
+    //   return {
+    //     bookTitle: title,
+    //     author: author,
+    //     link: link,
+    //   };
+    // });
+
+    // Convert the array of books into a JSON string
+    // const jsonOutput = JSON.stringify(books);
+
+    // console.log(jsonOutput);
+
+    return resA.output_text;
   }
 
   @ApiOperation({ summary: '추천받기' })
