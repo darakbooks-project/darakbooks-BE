a2ac8cae05b8e6f0b605b74229a1b7021c1f1311
shljessie <sl994@cornell.edu>
2023-06-19 17:49:41 +0300

    :memo: Swagger Documentation Update
    Updating Swagger documentation

--- src/gpt/dto/gpt.dto.ts
+++ src/gpt/dto/gpt.dto.ts
@@ -5,15 +5,15 @@ import { ApiProperty } from '@nestjs/swagger';
 export class gptDTO {
   @IsString()
   @ApiProperty({ example: '유현준의 인문 건축 기행', description: '책 제목' })
-  title: string;
+  Title: string;
 
   @IsString()
   @ApiProperty({ example: '유현준', description: '책 작가' })
-  author: string;
+  Author: string;
 
   @IsString()
   @ApiProperty({ example: '9788932423050', description: '책 isbn' })
-  isbn: string;
+  ISBN: string;
 
   @IsString()
   @ApiProperty({
@@ -21,13 +21,21 @@ export class gptDTO {
       'https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788932474892.jpg',
     description: '책표지 url',
   })
-  image: string;
+  Image: string;
 
   @IsString()
   @ApiProperty({
     example:
-      '이 책은 건축물 속에 담긴 독특한 생각들을 탐색하는 내용입니다. 요새 건축물과 예술에 관심이 생긴 당신에게 추천하는 책입니다.',
+      '이 책은 작가 유현준이 쓴 건축을 인문학적으로 바라본 전세계의 신기한 건물들에 관한 따뜻한 이야기입니다.',
+    description: '책 소개',
+  })
+  Intro: string;
+
+  @IsString()
+  @ApiProperty({
+    example:
+      '이 책은 건축물 속에 담긴 독특한 생각들을 탐색하는 내용입니다. 요새 건축물과 예술에 관심이 생긴 당신에게 적합하기때문에 추천하는 책입니다.',
     description: 'gpt가 이 책을 추천하는 이유',
   })
-  reason: string;
+  Reason: string;
 }
