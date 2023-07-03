ed76f237bd9ba3b5b61b453223e1d2aeebc1395c
shljessie <sl994@cornell.edu>
2023-07-03 13:01:27 +0900

    :memo: Update Swagger Documentation

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,22 @@ import cookieParser from 'cookie-parser';
 declare const module: any;
 
 async function bootstrap() {
-  
-  const app = await NestFactory.create(AppModule, 
-    {
-      cors:{
-        origin:'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization'] ,
-        credentials:true,
-        
-      }
-    });
-  
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  });
+
   app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle('다락책방 백엔드 API | DarakBooks Backend API')
-    .setDescription('다락책방 백엔드 독서 API 입니다. 로그인, 독서모임, 마이페이지, 책장추천, 독서기록, GPT 독서추천서비스를 제공하고 있습니다. | This is the backend API for DarakBooks. We are offering login, book clubs, my page, bookshelf recommendation, and GPT Book Recommendation Services.')
+    .setDescription(
+      '다락책방 백엔드 독서 API 입니다. 로그인, 독서모임, 마이페이지, 책장추천, 독서기록, GPT 독서추천서비스를 제공하고 있습니다. | This is the backend API for DarakBooks. We are offering login, book clubs, my page, bookshelf recommendation, and GPT Book Recommendation Services.',
+    )
     .setVersion('1.0.0')
     .addBasicAuth(
       {
